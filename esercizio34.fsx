292d340626fac668fa46491a19ef923f17e6af3b
eleonoradgr <eleonora.digregorio8@gmail.com>
2017-11-03 17:56:01 +0100

    impostazione iniziale

--- esercizio34.fsx
+++ esercizio34.fsx
@@ -1,1 +1,48 @@
+#load "mylwc.fsx"
+
+open System.Windows.Forms
+open System.Drawing
+open Mylwc
+
+let f = new Form(Text="MidTerm", TopMost=true)
+f.Show()
+
+type Letter () as this =
+    let mutable location = PointF()
+    let mutable size= SizeF()
+
+    let mutable char = char
+    let mutable font = Font()
+    let mutable color = Color()
+    let mutable mat = new W2V()
+    let mutable selected = true
+
+    member this.Location
+        with get() = location
+        and set(v) = location <- v;
+    member this.Size
+        with get() = size
+        and set(v) = size <- v;
+    member this.Font
+        with get() = size
+        and set(v) = size <- v;
+    member this.Color
+        with get() = color
+        and set(v) = color <- v;
+    member this.Mat
+        with get() = mat
+        and set(v) = mat <- v;
+    member this.Selected
+        with get() = mat
+        and set(v) = mat <- v;
+
+
+type Editor() as this=
+    inherit LWContainer()
+
+    
+
+
+
+
 
---
+++ mylwc.fsx
@@ -0,0 +1,203 @@
+open System.Windows.Forms
+open System.Drawing
+
+type W2V() =
+  let w2v = new Drawing2D.Matrix()
+  let v2w = new Drawing2D.Matrix()
+
+  member this.Translate(tx, ty) =
+    w2v.Translate(tx, ty)
+    v2w.Translate(-tx, -ty, Drawing2D.MatrixOrder.Append)
+
+  member this.Rotate(a) =
+    w2v.Rotate(a)
+    v2w.Rotate(-a, Drawing2D.MatrixOrder.Append)
+
+  member this.Scale(sx, sy) =
+    w2v.Scale(sx, sy)
+    v2w.Scale(1.f/sx, 1.f/sy, Drawing2D.MatrixOrder.Append)
+  
+  member this.W2V with get() = w2v
+  member this.V2W with get() = v2w
+
+let Rect2RectF (r:Rectangle) =
+  RectangleF(single r.X, single r.Y, single r.Width, single r.Height)
+
+let RectF2Rect (r:RectangleF) =
+  Rectangle(int r.X, int r.Y, int r.Width, int r.Height)
+
+type CoordinateType = View | World
+
+type LWControl() =
+  let mutable coordinates = View
+
+  let mutable location = PointF()
+  let mutable size = SizeF()
+
+  let mutable parent : Control = null
+
+  let clickevt = new Event<System.EventArgs>()
+  let mousedownevt = new Event<MouseEventArgs>()
+  let mousemoveevt = new Event<MouseEventArgs>()
+  let mouseupevt = new Event<MouseEventArgs>()
+
+  member this.CoordinateType
+    with get() = coordinates
+    and set(v) = coordinates <- v
+ 
+  member this.Position
+    with get() = location
+    and set(v) = location <- v
+
+  member this.Size
+    with get() = size
+    and set(v) = size <- v
+  
+  member this.Parent 
+    with get() = parent
+    and set(v) = parent <- v
+
+  member this.MouseDown = mousedownevt.Publish
+
+  abstract OnMouseDown : MouseEventArgs -> unit
+  default this.OnMouseDown e = mousedownevt.Trigger(e)
+
+  abstract OnMouseMove : MouseEventArgs -> unit
+  default this.OnMouseMove e = mousemoveevt.Trigger(e)
+
+  abstract OnMouseUp : MouseEventArgs -> unit
+  default this.OnMouseUp e = mouseupevt.Trigger(e)
+
+  abstract OnPaint : PaintEventArgs -> unit
+  default this.OnPaint e = ()
+
+
+  abstract HitTest : PointF -> bool
+  default this.HitTest p =
+    (new RectangleF(position, size)).Contains(p)
+
+type LWContainer() as this =
+  inherit UserControl()
+
+  let transformPoint (m:Drawing2D.Matrix) (p:PointF) =
+    let pts = [| p |]
+    m.TransformPoints(pts)
+    pts.[0]
+
+  let transform = W2V()
+
+  let controls = ResizeArray<LWControl>()
+
+  let scrollUp () =
+    transform.Translate(0.f, 10.f)
+    this.Invalidate()
+
+  
+  member this.LWControls with get() = controls
+
+  override this.OnMouseDown e =
+    let p = PointF(single e.X, single e.Y)
+    let controlsView = controls |> Seq.filter (fun c -> c.CoordinateType = View)
+    match (controlsView |> Seq.tryFind (fun c -> c.HitTest p)) with
+    | Some c -> c.OnMouseDown(e)
+    | None -> 
+      let pw = transformPoint transform.V2W p
+      let controlsWorld = controls |> Seq.filter (fun c -> c.CoordinateType = World)
+      match (controlsWorld |> Seq.tryFind(fun c -> c.HitTest pw)) with
+      | Some c -> c.OnMouseDown(e)
+      | None -> ()
+
+  override this.OnMouseMove e =
+    let p = PointF(single e.X, single e.Y)
+    let controlsView = controls |> Seq.filter (fun c -> c.CoordinateType = View)
+    match (controlsView |> Seq.tryFind (fun c -> c.HitTest p)) with
+    | Some c -> c.OnMouseMove(e)
+    | None -> ()
+
+  override this.OnMouseUp e =
+    let p = PointF(single e.X, single e.Y)
+    let controlsView = controls |> Seq.filter (fun c -> c.CoordinateType = View)
+    match (controlsView |> Seq.tryFind (fun c -> c.HitTest p)) with
+    | Some c -> c.OnMouseUp(e)
+    | None -> ()
+
+  override this.OnPaint e =
+    let g = e.Graphics
+
+    let t = g.Transform
+
+    g.Transform <- transform.W2V
+
+    for idx in (controls.Count - 1) .. -1 .. 0 do
+      let c = controls.[idx]
+      if c.CoordinateType = World then
+        c.OnPaint e
+    
+    g.Transform <- t
+
+    for idx in (controls.Count - 1) .. -1 .. 0 do
+      let c = controls.[idx]
+      if c.CoordinateType = View then
+        c.OnPaint e
+
+  override this.OnKeyDown e =
+    match e.KeyCode with
+    | Keys.W -> scrollUp()
+    | Keys.A -> 
+      transform.Translate(10.f, 0.f)
+      this.Invalidate()
+    | Keys.S -> 
+      transform.Translate(0.f, -10.f)
+      this.Invalidate()
+    | Keys.D ->
+      transform.Translate(-10.f, 0.f)
+      this.Invalidate()
+    | Keys.Q ->
+      transform.Rotate(10.f)
+      this.Invalidate()
+    | Keys.E ->
+      transform.Rotate(-10.f)
+      this.Invalidate()
+    | Keys.Z ->
+      transform.Scale(1.1f, 1.1f)
+      this.Invalidate()
+    | Keys.X ->
+      transform.Scale(1.f/1.1f, 1.f/1.1f)
+      this.Invalidate()
+    | _ -> ()
+
+type Rectbutton() as this =
+  
+  inherit LWControl()
+  
+  do this.Size <- SizeF(32.f, 32.f)
+  let mutable text = ""
+  let mutable selected = false
+  member this.Click = clickevt.Publish
+  member this.MouseDown = downevt.Publish
+  member this.MouseUp = upevt.Publish
+  member this.MouseMove = moveevt.Publish
+
+
+  member this.Text   
+   with get() = text
+   and set(v) = text <- v; this.Invalidate() // cerca di capire questo medoto e fai sotto uguale
+  
+  member this.Selected
+   with get() = selected
+   and set(v)= selected<-v;this.Invalidate() 
+
+  override this.OnMouseUp e = upevt.Trigger(e); clickevt.Trigger(new System.EventArgs())
+
+  override this.OnMouseMove e = moveevt.Trigger(e)
+
+  override this.OnMouseDown e = downevt.Trigger(e)
+
+  override this.OnPaint e =
+    let g = e.Graphics
+    if selected
+        then g.FillRectangle(Brushes.Orange, new Rectangle(0,0, int(this.Size.Width),int(this.Size.Height)) );
+    else g.FillRectangle( Brushes.Khaki , new Rectangle(0,0, int(this.Size.Width),int(this.Size.Height)));
+    let sz = g.MeasureString(text, this.Parent.Font)
+    g.DrawString(text, this.Parent.Font, Brushes.Black, PointF((this.Size.Width - sz.Width) / 2.f, (this.Size.Height - sz.Height) / 2.f))
+ 
